7f74e79e9e36044edb20d6496b3888d32687ad90
kgodfr <132689456+kgodfr@users.noreply.github.com>
2023-06-08 15:20:25 +0000

    Update EgaApiApplicationTests.java

--- src/test/java/com/apimasterucao/EgaAPI/EgaApiApplicationTests.java
+++ src/test/java/com/apimasterucao/EgaAPI/EgaApiApplicationTests.java
@@ -3,6 +3,11 @@ package com.apimasterucao.EgaAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * @author TETE Kodjovi
+ *
+ */
+
 @SpringBootTest
 class EgaApiApplicationTests {
 
