5f2797bf495ce2b86c3e1bf757c191e5e064bfcf
kgodfr <132689456+kgodfr@users.noreply.github.com>
2023-06-08 15:17:33 +0000

    Update ClientRepository.java

--- src/main/java/com/apimasterucao/EgaAPI/repository/ClientRepository.java
+++ src/main/java/com/apimasterucao/EgaAPI/repository/ClientRepository.java
@@ -4,6 +4,11 @@ import com.apimasterucao.EgaAPI.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * @author TETE Kodjovi
+ *
+ */
+
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer> {
 }
