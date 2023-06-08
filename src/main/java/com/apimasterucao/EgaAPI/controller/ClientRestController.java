e8309765875b893277bb5166da09c5bc5eb95477
kgodfr <132689456+kgodfr@users.noreply.github.com>
2023-06-08 15:15:43 +0000

    Update ClientRestController.java

--- src/main/java/com/apimasterucao/EgaAPI/controller/ClientRestController.java
+++ src/main/java/com/apimasterucao/EgaAPI/controller/ClientRestController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * @author TETE Kodjovi
+ *
+ */
+
 @RestController
 public class ClientRestController {
     @Autowired
